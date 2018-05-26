27e9303f1eb76c90b3a08c8d38741b750d9a8ccd
Taku Funami <fff273109@gmail.com>
2018-05-26 16:08:39 +0900

    ray_cast_test プロジェクト　初回コミット
    Raycast のテスト用に作成したプロジェクトです。
    マウスクリックしたときだけにRayを飛ばし、そのRayが衝突したオブジェクトの情報を取得するようにしたいのですが、
    現在マウスオーバーしたときにオブジェクトの情報を取得するようになってしまっています。

---
+++ Assets/cube_con.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_con : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        //メインカメラ上で　カーソルのあった場所　場所にRayを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+        {
+            //Rayが当たるオブジェクトがあった場合はそのオブジェクト名をログに表示
+            Debug.Log(hit.collider.gameObject.name);
+        }
+
+    }
+}
---
+++ Assets/cube_con_2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cube_con_2 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start() { }
+
+    //クリックしたオブジェクトを消す
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject,0.5f);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
